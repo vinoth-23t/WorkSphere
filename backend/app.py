bd88a4e014c4617fb18615b40fa8e3ed1102fadd
vinoth <vinoth5019t@gmail.com>
2026-05-25 14:30:02 +0530

    added postgresql

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
 @app.route("/")
 def home():
-    return {"message": "Backend Running"}
+    return {"message": "Backend Running with PostgreSQL"}
 
 if __name__ == "__main__":
     app.run(debug=True)
